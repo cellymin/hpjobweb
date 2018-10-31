d7909a2ff2a1e849ad2899eba24b6c1d1469824c
cellymin <1174064375@qq.com>
2018-10-31 16:34:47 +0800

    update .gitignore

--- lcphp/org/swfupload250/lc_delfile.php
+++
@@ -1,1 +0,0 @@
-<?phpC("debug", 0);$file = urldecode($_POST["file"]);$file = explode("@@@", $file);if ($file) {    foreach ($file as $v) {        @unlink($v);    }    echo 1;} else {    echo 0;}?>
--- web/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!defined("PATH_LC")){exit('拒绝访问');}
-$db_config=include PATH_ROOT.'/config/database.php';
-$app_config=include PATH_ROOT.'/config/app.php';
-return array_merge($db_config,$app_config);
-?>
